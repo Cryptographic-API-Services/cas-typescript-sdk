973c25614d9d1f98fb1a6f16e0e5bfbf15dd5ef8
Mike Mulchrone <mtmulch0191@outlook.com>
2024-01-13 11:17:54 -0500

    adding argon2 wrapper

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,5 @@
-import { EASConfiguration } from "./EASConfiguration";
-import { TokenCache } from "./cache/token-cache";
-import { ED25519DalekService } from "./services/ed25519-dalek-services";
-import { PasswordService } from "./services/password-service";
-import { RsaService } from "./services/rsa-service";
-import { SignatureService } from "./services/signature-service";
-import { SymmetricEncryptionService } from "./services/symmetric-encryption-service";
-import { HybridEncryptionService } from "./services/hybrid-encryption-service";
-
+import { Argon2Wrapper } from "./password-hashers/argon2-wrapper";
 
 export {
-    EASConfiguration,
-    TokenCache,
-    PasswordService,
-    RsaService,
-    ED25519DalekService,
-    SignatureService,
-    SymmetricEncryptionService,
-    HybridEncryptionService
-};
+    Argon2Wrapper
+}
--- src/password-hashers/argon2-wrapper.ts
+++ src/password-hashers/argon2-wrapper.ts
@@ -1,15 +1,18 @@
 import ffi from "ffi-napi";
 
 export class Argon2Wrapper {
-    private ffiLibraries;
+  private ffiLibraries;
 
-    constructor() {
-        this.ffiLibraries = ffi.Library("./cas_core_lib.dll", {
-            "argon2_hash": ["string", ["string"]]
-        })
-    }
+  constructor() {
+    this.ffiLibraries = ffi.Library("./cas_core_lib.dll", {
+      "argon2_hash": ["string", ["string"]],
+    });
+  }
 
-    public hashPassword(passwordToHash: string): string | null{
-        return this.ffiLibraries.argon2_hash(pas)
+  public hashPassword(passwordToHash: string): string | null {
+    if (!passwordToHash) {
+        throw new Error("You must provide a password to hash");
     }
+    return this.ffiLibraries.argon2_hash(passwordToHash);
+  }
 }
