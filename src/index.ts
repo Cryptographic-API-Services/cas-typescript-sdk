59ab9ba3dc137c82a4893dfe86346e60da922670
Mike Mulchrone <mtmulch0191@outlook.com>
2024-01-13 11:40:09 -0500

    testing

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { Argon2Wrapper } from "./password-hashers/argon2-wrapper";
 
+let wrapper = new Argon2Wrapper();
+let hash = wrapper.hashPassword('testing');
+console.log(hash);
+
 export {
     Argon2Wrapper
 }
--- src/password-hashers/argon2-wrapper.ts
+++ src/password-hashers/argon2-wrapper.ts
@@ -1,12 +1,21 @@
 import ffi from "ffi-napi";
+import os from "os";
 
 export class Argon2Wrapper {
   private ffiLibraries;
 
   constructor() {
-    this.ffiLibraries = ffi.Library("./cas_core_lib.dll", {
-      "argon2_hash": ["string", ["string"]],
-    });
+    let platform = os.platform();
+    console.log(platform);
+    if (platform === "win32") {
+        this.ffiLibraries = ffi.Library("./cas_core_lib.dll", {
+          "argon2_hash": ["string", ["string"]],
+        });
+    } else {
+        this.ffiLibraries = ffi.Library("./libcas_core_lib.so", {
+            "argon2_hash": ["string", ["string"]],
+          });
+    }
   }
 
   public hashPassword(passwordToHash: string): string | null {
