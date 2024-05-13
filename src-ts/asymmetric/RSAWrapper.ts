c9e1933848b3fb5fee2d712366a03ef9ed8f84f6
WingZer0o <mtmulch0191@outlook.com>
2024-05-13 19:01:42 -0400

    #21 rsa wrapper jsdoc

--- src-ts/asymmetric/RSAWrapper.ts
+++ src-ts/asymmetric/RSAWrapper.ts
@@ -1,12 +1,25 @@
 import { decryptCiphertextRsa, encryptPlaintextRsa, generateRsaKeys, RsaKeyPairResult, signRsa, verifyRsa } from "../../index";
 
 export class RSAWrapper {
+
+  /**
+   * Generates an RSA key pair based of parameter sent in 1024, 2048, and 4096 are supported.
+   * @param keySize 
+   * @returns RsaKeyPairResult
+   */
   public generateKeys(keySize: number): RsaKeyPairResult {
     if (keySize !== 1024 && keySize !== 2048 && keySize !== 4096) {
         throw new Error("You must provide an appropriate key size to generate RSA keys");
     }
     return generateRsaKeys(keySize);
   }
+
+  /**
+   * Encrypts a plaintext byte array with a RSA public key
+   * @param publicKey 
+   * @param plaintext 
+   * @returns Array<number>
+   */
 
   public encrypt(publicKey: string, plaintext: Array<number>): Array<number> {
     if (!publicKey) {
@@ -18,6 +31,13 @@ export class RSAWrapper {
     return encryptPlaintextRsa(publicKey, plaintext);
   }
 
+  /**
+   * Decrypts a ciphertext with an RSA private key.
+   * @param privateKey 
+   * @param ciphertext 
+   * @returns Array<number>
+   */
+
   public decrypt(privateKey: string, ciphertext: Array<number>): Array<number> {
     if (!privateKey) {
       throw new Error("You must provide a private key to encrypt with RSA");
@@ -28,16 +48,29 @@ export class RSAWrapper {
     return decryptCiphertextRsa(privateKey, ciphertext);
   }
 
-  public sign(privateKey: string, hash: Array<number>): Array<number> {
+  /**
+   * Signs a byte array with an RSA private key for verification.
+   * @param privateKey 
+   * @param hash 
+   * @returns Array<number>
+   */
+  public sign(privateKey: string, dataToSign: Array<number>): Array<number> {
     if (!privateKey) {
       throw new Error("You must provide a private key to sign with RSA");
     }
-    if (!hash || hash.length === 0) {
+    if (!dataToSign || dataToSign.length === 0) {
       throw new Error("You must provide an allocated hash to sign with RSA");
     }
-    return signRsa(privateKey, hash);
+    return signRsa(privateKey, dataToSign);
   }
 
+  /**
+   * Verifies signed data by the corresponding private key with an RSA public key.
+   * @param publicKey 
+   * @param hash 
+   * @param signature 
+   * @returns boolean
+   */
   public verify(publicKey: string, hash: Array<number>, signature: Array<number>): boolean {
     if (!publicKey) {
       throw new Error("You must provide a public key to verify with RSA");
