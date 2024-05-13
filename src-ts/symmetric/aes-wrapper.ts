c0a292dc5cf2ca94df718785632ebfd9511934a5
WingZer0o <mtmulch0191@outlook.com>
2024-05-13 19:31:09 -0400

    aes wrapper jsdoc

--- src-ts/symmetric/aes-wrapper.ts
+++ src-ts/symmetric/aes-wrapper.ts
@@ -41,30 +41,72 @@ export class AESWrapper {
         return aes256Key();
     }
 
+    /**
+     * Generates an 96 bit AES nonce
+     * @returns Array<number>
+     */
     public generateAESNonce(): Array<number> {
         return aesNonce();
     }
 
+    /**
+     * Encrypts with AES 128.
+     * @param aesKey 
+     * @param nonce 
+     * @param plaintext 
+     * @returns Array<number>
+     */
     public aes128Encrypt(aesKey: Array<number>, nonce: Array<number>, plaintext: Array<number>): Array<number> {
         return aes128Encrypt(aesKey, nonce, plaintext);
     }
 
+    /**
+     * Decrypts with AES 128
+     * @param aesKey 
+     * @param nonce 
+     * @param ciphertext 
+     * @returns Array<number>
+     */
     public aes128Decrypt(aesKey: Array<number>, nonce: Array<number>, ciphertext: Array<number>): Array<number> {
         return aes128Decrypt(aesKey, nonce, ciphertext);
     }
 
+    /**
+     * Encrypts with AES-256
+     * @param aesKey 
+     * @param nonce 
+     * @param plaintext 
+     * @returns 
+     */
     public aes256Encrypt(aesKey: Array<number>, nonce: Array<number>, plaintext: Array<number>): Array<number> {
         return aes256Encrypt(aesKey, nonce, plaintext);
     }
 
+    /**
+     * Decrypts with AES 256
+     * @param aesKey 
+     * @param nonce 
+     * @param ciphertext 
+     * @returns 
+     */
     public aes256Decrypt(aesKey: Array<number>, nonce: Array<number>, ciphertext: Array<number>): Array<number> {
         return aes256Decrypt(aesKey, nonce, ciphertext);
     }
 
+    /**
+     * Derives an AES-256 key from a X25519 Diffie Hellman shared secret.
+     * @param shared_secret 
+     * @returns 
+     */
     public aes256KeyNonceX25519DiffieHellman(shared_secret: Array<number>): AesKeyFromX25519SharedSecret {
          return aes256KeyFromX25519SharedSecret(shared_secret);
     }
 
+    /**
+     * Derives an AES-128 key from a X25519 Diffie Hellman shared secret.
+     * @param shared_secret 
+     * @returns 
+     */
     public aes128KeyNonceX25519DiffieHellman(shared_secret: Array<number>): AesKeyFromX25519SharedSecret {
         return aes128KeyFromX25519SharedSecret(shared_secret);
    }
