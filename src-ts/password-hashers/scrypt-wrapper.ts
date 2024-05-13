dfe394f560d61a3d02b9f756693fb49bb4db8f3d
WingZer0o <mtmulch0191@outlook.com>
2024-05-13 19:23:41 -0400

    password hasher jsdoc

--- src-ts/password-hashers/scrypt-wrapper.ts
+++ src-ts/password-hashers/scrypt-wrapper.ts
@@ -3,6 +3,12 @@ import { IPasswordHasherBase } from "./password-hasher-base";
 
 export class ScryptWrapper implements IPasswordHasherBase {
 
+  /**
+   * Verifies a password with SCrypt on the threadpool.
+   * @param hashedPassword 
+   * @param passwordToCheck 
+   * @returns boolean
+   */
   verifyThreadPool(hashedPassword: string, passwordToCheck: string): boolean {
     if (!hashedPassword || !passwordToCheck) {
       throw new Error(
@@ -12,6 +18,11 @@ export class ScryptWrapper implements IPasswordHasherBase {
     return scryptVerifyThreadpool(hashedPassword, passwordToCheck);
   }
 
+  /**
+   * Hashes a password with SCrypt on the threadpool.
+   * @param password 
+   * @returns string
+   */
   hashPasswordThreadPool(password: string): string {
     if (!password) {
       throw new Error("You must provide a password to hash with Scrypt");
@@ -19,6 +30,11 @@ export class ScryptWrapper implements IPasswordHasherBase {
     return scryptHashThreadpool(password);
   }
 
+  /**
+   * Hashes a password with SCrypt
+   * @param password 
+   * @returns string
+   */
   public hashPassword(password: string): string {
     if (!password) {
       throw new Error("You must provide a password to hash with Scrypt");
@@ -26,6 +42,12 @@ export class ScryptWrapper implements IPasswordHasherBase {
     return scryptHash(password);
   }
 
+  /**
+   * Verifies that a password is the same as the hashed password with SCrypt.
+   * @param hashedPassword 
+   * @param passwordToVerify 
+   * @returns boolean
+   */
   public verify(hashedPassword: string, passwordToVerify: string): boolean {
     if (!hashedPassword || !passwordToVerify) {
       throw new Error(
