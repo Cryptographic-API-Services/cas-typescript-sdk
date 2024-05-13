03980b243516bd754336d0956c37c140e67ddb30
WingZer0o <mtmulch0191@outlook.com>
2024-05-13 19:08:18 -0400

    digital signature jsdocs

--- src-ts/digital-signature/digital-signaturte-sha-256.ts
+++ src-ts/digital-signature/digital-signaturte-sha-256.ts
@@ -3,6 +3,11 @@ import { IDigitalSignature } from "./digital-signature-base";
 
 export class DigitalSignatureSHA256Wrapper implements IDigitalSignature {
     
+    /**
+     * Creates an ED25519 siganture from an array of bytes with SHA3-512.
+     * @param dataToSign 
+     * @returns SHAED25519DalekDigitalSignatureResult
+     */
     createED25519(dataToSign: number[]): Shaed25519DalekDigitalSignatureResult {
         if (dataToSign?.length === 0) {
             throw new Error("Must provide allocated data to sign");
@@ -10,6 +15,13 @@ export class DigitalSignatureSHA256Wrapper implements IDigitalSignature {
         return sha256Ed25519DigitalSignature(dataToSign);
     }
 
+    /**
+     * Verifies an ED25519 signature with the public key generated from running createED25519() with SHA3-512
+     * @param publicKey 
+     * @param dataToVerify 
+     * @param signature 
+     * @returns boolean
+     */
     verifyED25519(publicKey: number[], dataToVerify: number[], signature: number[]): boolean {
         if (!publicKey) {
             throw new Error("You must provide a public key for verify with ED25519");
@@ -23,6 +35,12 @@ export class DigitalSignatureSHA256Wrapper implements IDigitalSignature {
         return sha256Ed25519DigitalSignatureVerify(publicKey, dataToVerify, signature);
     }
 
+    /**
+     * Generates and RSA digital signature with SHA3-512
+     * @param rsa_key_size 
+     * @param data_to_sign 
+     * @returns RsaDigitalSignatureResult
+     */
     createRsa(rsa_key_size: number, data_to_sign: number[]): RsaDigitalSignatureResult {
         if (rsa_key_size !== 1024 && rsa_key_size !== 2048 && rsa_key_size !== 4096) {
             throw new Error("You need to provide an appropriate RSA key size.");
@@ -33,6 +51,13 @@ export class DigitalSignatureSHA256Wrapper implements IDigitalSignature {
         return sha256RsaDigitalSignature(rsa_key_size, data_to_sign);
     }
     
+    /**
+     * Verifies a digital signature created with the RSA public key.
+     * @param public_key 
+     * @param data_to_verify 
+     * @param signature 
+     * @returns boolean
+     */
     verifyRSa(public_key: string, data_to_verify: number[], signature: number[]): boolean {
         if (!public_key) {
             throw new Error("Must provide a public key");
