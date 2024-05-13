a595a487343b447e005dd11df3908236a3bb8f44
WingZer0o <mtmulch0191@outlook.com>
2024-05-13 19:16:05 -0400

    hybrid encryption jsdoc

--- src-ts/helpers/nonce-generator.ts
+++ src-ts/helpers/nonce-generator.ts
@@ -2,6 +2,10 @@ import * as crypto from "crypto";
 
 export class NonceGenerator {
 
+    /**
+     * Generates a unique cryptographic 96-bit nonce for usage with AES-128-GCM and AES-256-GCM.
+     * @returns string
+     */
     public generateNonce(): string {
         const nonceBytes = crypto.randomBytes(12);
         return nonceBytes.toString('hex').substring(0, 12);
