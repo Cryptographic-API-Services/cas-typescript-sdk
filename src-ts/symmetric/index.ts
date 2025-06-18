b831b89877a1b469baead1f35ac599df12369adf
Mike Mulchrone <mikemulchrone987@gmail.com>
2025-06-17 21:23:56 -0400

    types import

--- src-ts/symmetric/index.ts
+++ src-ts/symmetric/index.ts
@@ -1,4 +1,4 @@
-import { CASAesKeyFromX25519SharedSecret } from "../../index";
+import { CASAesKeyFromX25519SharedSecret } from "../../index.d";
 import { AESWrapper } from "./aes-wrapper";
 
 export { AESWrapper, CASAesKeyFromX25519SharedSecret };
