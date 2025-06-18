73b0c6f07f314f4913aabdcb5062e334b403f2b1
Mike Mulchrone <mikemulchrone987@gmail.com>
2025-06-17 21:40:28 -0400

    index

--- src-ts/key_exchange/index.ts
+++ src-ts/key_exchange/index.ts
@@ -1,4 +1,4 @@
-import { CASx25519SecretPublicKeyResult } from "../../index.d.d";
+import { CASx25519SecretPublicKeyResult } from "../../index.d";
 import { X25519Wrapper } from "./x25519";
 
 export { X25519Wrapper, CASx25519SecretPublicKeyResult };
