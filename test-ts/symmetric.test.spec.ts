df1c6adb7775282244558f497869b04e60c6752a
Mike Mulchrone <mikemulchrone987@gmail.com>
2025-07-05 18:25:18 -0400

    adding x25519 key exchange for aes-128 and aes-256 encryption to test if the underlying library is working

--- test-ts/symmetric.test.spec.ts
+++ test-ts/symmetric.test.spec.ts
@@ -1,5 +1,6 @@
 import { assert } from "chai";
 import { AESWrapper } from "../src-ts/symmetric/aes-wrapper";
+import { X25519Wrapper } from '../src-ts/key_exchange/x25519';
 import { areEqual } from "./helpers/array";
 
 describe("Symmetric Tests", () => {
@@ -26,6 +27,52 @@ describe("Symmetric Tests", () => {
     const ciphertext = aesWrapper.aes256Encrypt(aesKey, aesNonce, tohashBytes);
     const plaintxt = aesWrapper.aes256Decrypt(aesKey, aesNonce, ciphertext);
     var result = areEqual(plaintxt, tohashBytes);
+    assert.isTrue(result);
+  });
+
+  it("ase 256 X25519 Diffie-Hellman encrypt and decrypt", () => {
+    const x25519 = new X25519Wrapper();
+    const alice= x25519.generateSecretAndPublicKey();
+    const bob = x25519.generateSecretAndPublicKey();
+
+    const aliceSharedSecret = x25519.generateSharedSecret(alice.secretKey, bob.publicKey);
+    const bobSharedSecret = x25519.generateSharedSecret(bob.secretKey, alice.publicKey);
+
+    const aesWrapper: AESWrapper = new AESWrapper();
+    const aliceAesKey = aesWrapper.aes256KeyNonceX25519DiffieHellman(aliceSharedSecret);
+    const bobAesKey = aesWrapper.aes256KeyNonceX25519DiffieHellman(bobSharedSecret);
+
+    const tohashed: string = "This is my array to encrypt";
+    const encoder = new TextEncoder();
+    const tohashBytes: Array<number> = Array.from(encoder.encode(tohashed));
+
+    const aliceCiphertext = aesWrapper.aes256Encrypt(aliceAesKey.aesKey, aliceAesKey.aesNonce, tohashBytes);
+    const bobPlaintext = aesWrapper.aes256Decrypt(bobAesKey.aesKey, aliceAesKey.aesNonce, aliceCiphertext);
+
+    var result = areEqual(bobPlaintext, tohashBytes);
+    assert.isTrue(result);
+  });
+
+  it("ase 128 X25519 Diffie-Hellman encrypt and decrypt", () => {
+    const x25519 = new X25519Wrapper();
+    const alice= x25519.generateSecretAndPublicKey();
+    const bob = x25519.generateSecretAndPublicKey();
+
+    const aliceSharedSecret = x25519.generateSharedSecret(alice.secretKey, bob.publicKey);
+    const bobSharedSecret = x25519.generateSharedSecret(bob.secretKey, alice.publicKey);
+
+    const aesWrapper: AESWrapper = new AESWrapper();
+    const aliceAesKey = aesWrapper.aes128KeyNonceX25519DiffieHellman(aliceSharedSecret);
+    const bobAesKey = aesWrapper.aes128KeyNonceX25519DiffieHellman(bobSharedSecret);
+
+    const tohashed: string = "This is my array to encrypt";
+    const encoder = new TextEncoder();
+    const tohashBytes: Array<number> = Array.from(encoder.encode(tohashed));
+
+    const aliceCiphertext = aesWrapper.aes128Encrypt(aliceAesKey.aesKey, aliceAesKey.aesNonce, tohashBytes);
+    const bobPlaintext = aesWrapper.aes128Decrypt(bobAesKey.aesKey, aliceAesKey.aesNonce, aliceCiphertext);
+
+    var result = areEqual(bobPlaintext, tohashBytes);
     assert.isTrue(result);
   });
 });
